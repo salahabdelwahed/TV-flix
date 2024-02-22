46832a8be83d62c3c4e0249213abb088f46ec5e2
Your Name <Your email>
2024-02-22 13:30:23 +0100

    New update

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,14 +1,13 @@
 import React from "react";
-import "../CSS/homePage.css"; 
-import CardFilm from "./CardFilm.js"
-import dataUpComing from '../DB/dataUpcomingFilm.js'
-import dataThisWeek from "../DB/dataThisweek.js"
+import CardFilm from "./CardFilm.js";
+import dataUpComing from "../DB/dataUpcomingFilm.js";
+import dataThisWeek from "../DB/dataThisweek.js";
 import dataTopTreending from "../DB/dataTopTreending.js";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
+import "../CSS/homePage.css";
+import "../CSS/Carous.css";
 
-
-function HomePage() { 
-
+function HomePage() {
   return (
     <div className="overaly">
       <div className="overaly-block1">
@@ -61,68 +60,77 @@ function HomePage() {
         <div className="crous">
           <Carousel data-bs-theme="dark">
             <Carousel.Item>
-              <img
-                className="d-block w-100"
+              <img 
+                className=" Img d-block w-100"
                 src="https://image.tmdb.org/t/p/w1280/4MCKNAc6AbWjEsM2h9Xc29owo4z.jpg"
                 alt="First slide"
               />
-              <Carousel.Caption>
-                <h5>The Beekeeper</h5>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
+              <Carousel.Caption className="crous-info">
+              <h5 className="crous-tilte">The Beekeeper</h5>
+                <p className="crous-decr">
+                  One man's campaign for vengeance takes on national stakes
+                  after he is revealed to be a former operative of a powerful
+                  and clandestine organization known as Beekeepers...
+                </p> 
+                <Button className='btn-x' variant="danger">Watch Now</Button>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
+                className=" Img d-block w-100"
                 src="https://image.tmdb.org/t/p/w1280/criPrxkTggCra1jch49jsiSeXo1.jpg"
                 alt="Second slide"
               />
-              <Carousel.Caption>
-                <h5>Second slide label</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Carousel.Caption className="crous-info">
+                <h5  className="crous-tilte">The Marvels</h5>
+                <p  className="crous-decr">
+                  Carol Danvers, aka Captain Marvel, has reclaimed her identity
+                  from the tyrannical Kree and taken revenge on the Supreme
+                  Intelligence...
+                </p>
+                <Button className='btn-x' variant="danger">Watch Now</Button>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
+                className="Img d-block w-100"
                 src="https://image.tmdb.org/t/p/w1280/pWsD91G2R1Da3AKM3ymr3UoIfRb.jpg"
                 alt="Third slide"
               />
-              <Carousel.Caption>
-                <h5>Third slide label</h5>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
+              <Carousel.Caption className="crous-info">
+                <h5 className="crous-tilte">Badland Hunters</h5>
+                <p  className="crous-decr">
+                  After a deadly earthquake turns Seoul into a lawless badland,
+                  a fearless huntsman springs into action to rescue...
                 </p>
+                <Button className='btn-x' variant="danger">Watch Now</Button>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
-        </div> 
-        <div className="movieUpcoming">
-         <h1 className="head">Upcoming Movies</h1>
-        <div className="allCards"> 
-        {dataUpComing.map((Movie) => (
-          <CardFilm key={Movie.id} Movie={Movie} />
-        ))}
         </div>
+        <div className="movieUpcoming">
+          <h1 className="head">Upcoming Movies</h1>
+          <div className="allCards">
+            {dataUpComing.map((Movie) => (
+              <CardFilm key={Movie.id} Movie={Movie} />
+            ))}
+          </div>
         </div>
         <div className="thisweek">
-        <h1 className="head">This week</h1> 
-        <div className="allCards"> 
-        {dataThisWeek.map((film) => (
-          <CardFilm key={film.id} Movie={film} />
-        ))}
-        </div>
+          <h1 className="head">This week</h1>
+          <div className="allCards">
+            {dataThisWeek.map((film) => (
+              <CardFilm key={film.id} Movie={film} />
+            ))}
+          </div>
         </div>
         <div className="top treending">
-        <h1 className="head">Top treending</h1> 
-        <div className="allCards"> 
-        {dataTopTreending.map((movie) => (
-          <CardFilm key={movie.id} Movie={movie} />
-        ))}
-        </div>
+          <h1 className="head">Top treending</h1>
+          <div className="allCards">
+            {dataTopTreending.map((movie) => (
+              <CardFilm key={movie.id} Movie={movie} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
